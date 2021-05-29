a28a3bf91a9db3287a13de54be7fee2d9ff2261b
Vibi Primantono <vibi94@gmail.com>
2021-05-30 00:00:45 +0700

    add route /discount/venue

--- route/index.ts
+++ route/index.ts
@@ -1,12 +1,16 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
 import { profileIndex } from '../controller/profileController';
+import { discounteGet } from '../controller/discountController';
 
 // Route Declare
 const route = express.Router();
 
 // Route List
 route.get('/', profileIndex);
+
+// Route Discount List
+route.get('/venue', discounteGet);
 
 // health check api
 route.get('/health-check', (req: Request, res: Response) => {
